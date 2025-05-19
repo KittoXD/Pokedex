23e4274bd7cacb2bcec8ca0520ae781484708a5d
Gabriel L.S <amvbiel@gmail.com>
2025-05-19 19:41:20 -0300

    Update HomeView.vue

--- pokedex - Gabriel/src/views/HomeView.vue
+++ pokedex - Gabriel/src/views/HomeView.vue
@@ -1,11 +1,10 @@
 <template>
   <div class="min-h-screen bg-gray flex flex-col items-center justify-center px-4">
-    <h1 class="text-4xl font-extrabold text-sky-600 mb-6">Bem-vindo(a) ao meu App</h1>
+    <h1 class="text-5xl font-extrabold text-gray-600 mb-6">Bem-vindo(a) ao Meu App</h1>
+    
+    <p class="text-lg text-gray-700 mb-8 text-center max-w-md">
+      Esta é a página inicial. Aqui você pode navegar, acessar funcionalidades e conhecer o sistema.
+    </p>
 
-    <div class="grid grid-cols-1 sm:grid-cols-2 gap-4">
-    </div>
   </div>
 </template>
-
-<script setup>
-</script>
