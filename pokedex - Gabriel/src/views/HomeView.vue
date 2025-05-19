9f7ffe46beb76a776506682bd9d3b8e7d73d9bd1
Gabriel L.S <amvbiel@gmail.com>
2025-05-19 19:36:00 -0300

    Update HomeView.vue
    adicionei á home um titulo no centro

--- pokedex - Gabriel/src/views/HomeView.vue
+++ pokedex - Gabriel/src/views/HomeView.vue
@@ -1,5 +1,11 @@
 <template>
-  <div>
-    <h1>Início</h1>
+  <div class="min-h-screen bg-gray flex flex-col items-center justify-center px-4">
+    <h1 class="text-4xl font-extrabold text-sky-600 mb-6">Bem-vindo(a) ao meu App</h1>
+
+    <div class="grid grid-cols-1 sm:grid-cols-2 gap-4">
+    </div>
   </div>
 </template>
+
+<script setup>
+</script>
